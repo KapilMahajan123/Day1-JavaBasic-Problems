5902ed6695f8b15d07181a253099912c39f42ad0
KapilMahajan123 <111445779+KapilMahajan123@users.noreply.github.com>
2022-11-21 10:26:51 +0530

    Displaying the Massage

--- src/com/day1problems/java/DisMsg.java
+++ src/com/day1problems/java/DisMsg.java
@@ -1,8 +1,15 @@
 package com.day1problems.java;
 
+/**
+ * 
+ * @author Kapil
+ *
+ */
 public class DisMsg {
 	
 	 public static void main(String args[])
+	   
+	   // printing the massage	 
 	   {
 	        System.out.println("Hello!.. Kapil");   
 	   }
